98fd5302834475eac489970670bf1cf69ae04632
Taras Yatsyshyn <yatarasis@gmail.com>
2017-10-20 01:32:08 +0300

    configured routing

--- src/EPA.Web/ClientApp/components/Layout.tsx
+++ src/EPA.Web/ClientApp/components/Layout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { NavMenu } from './NavMenu';
+import { NavigationMenu } from './NavigationMenu';
+import { Footer } from './Footer';
 
 export interface LayoutProps {
     children?: React.ReactNode;
@@ -8,14 +9,9 @@ export interface LayoutProps {
 export class Layout extends React.Component<LayoutProps, {}> {
     public render() {
         return <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-sm-3'>
-                    <NavMenu />
-                </div>
-                <div className='col-sm-9'>
-                    { this.props.children }
-                </div>
-            </div>
+            <NavigationMenu />
+            {this.props.children}
+            <Footer />
         </div>;
     }
 }
--- src/EPA.Web/ClientApp/routes.tsx
+++ src/EPA.Web/ClientApp/routes.tsx
@@ -4,9 +4,11 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { DateSync } from './components/DateSync';
 import { Counter } from './components/Counter';
+import { ProfTest } from './components/ProfTest';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={ Counter } />
     <Route path='/date' component={DateSync} />
+    <Route path='/profTest' component={ProfTest} />
 </Layout>;
