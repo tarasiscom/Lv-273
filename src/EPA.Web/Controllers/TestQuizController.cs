1ad863edd7158ea84fbc33614046184b9585acb5
astetskovych <astetskovych@gmail.com>
2017-10-21 19:39:36 +0300

    TestQuiz_API controller

---
+++ src/EPA.Web/Controllers/TestQuizController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EPA.Common.Interfaces;
+using EPA.Common.DTO;
+using EPA.Common.dto.CommonQuiz;
+
+namespace EPA.Web.Controllers
+{
+    //[Produces("application/json")]
+    public class TestQuizController : Controller
+    {
+        private IAccessToQuestionsByNameTest profTestQuestionsProvider;
+
+        public TestQuizController(IAccessToQuestionsByNameTest profTestQuestionsProvider)
+        {
+            this.profTestQuestionsProvider = profTestQuestionsProvider;
+        }
+
+        /// <summary>
+        /// This method retrieves list of tests
+        /// </summary>
+        /// <returns>collection of tests</returns>
+        [Route("/api/profTest")]
+        public IEnumerable<CommonTestList> GetTestList() => profTestQuestionsProvider.GetTestList();
+
+        /// <summary>
+        /// This method retrieves list of questions
+        /// </summary>
+        /// <param name="testId">id of selected test</param> 
+        /// <returns>collection of questions</returns>
+        [Route("/api/profTest/{testId}/questions")]
+        public IEnumerable<CommonQuestions> GetQuestions(int testId) => profTestQuestionsProvider.GetQuestionByListID(testId);
+
+
+        /// <summary>
+        /// This method retrieves list of answers
+        /// </summary>
+        /// <param name="questionId">id of question</param>
+        /// <returns>collection of answers</returns>
+        [Route("/api/profTest/{testId}/{id}/answers")]
+        public IEnumerable<CommonAnswers> GetAnswers(int questionId) => profTestQuestionsProvider.GetAnswersByQuestId(questionId);
+    }
+}
--- src/EPA.Web/Startup.cs
+++ src/EPA.Web/Startup.cs
@@ -29,6 +29,7 @@ namespace EPA.Web
             services.AddMvc();
             services.AddSingleton<ILastSyncProvider, DateAccess>();
             services.AddTransient<IProfTestInfoProvider, ProfTestInfoProvider>();
+            services.AddTransient<IAccessToQuestionsByNameTest, AccessToQuestions>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
