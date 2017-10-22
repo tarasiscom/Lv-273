05acc36100a941c6346ea45ed87e47b24959045b
Taras Yatsyshyn <yatarasis@gmail.com>
2017-10-23 01:47:34 +0300

    services configuration

--- src/EPA.Web/Startup.cs
+++ src/EPA.Web/Startup.cs
@@ -29,6 +29,7 @@ namespace EPA.Web
             services.AddMvc();
             services.AddSingleton<ILastSyncProvider, DateAccess>();
             services.AddTransient<IProfTestInfoProvider, ProfTestInfoProvider>();
+        //  services.AddTransient<IProfTestResultProvider, ProfTestResultProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
