7beae163e3924ebcb50e36d6079df771292bdf0b
Buchkovska <hjvfydthtvtyxbr>
2017-10-23 18:46:50 +0300

    add automapper

---
+++ src/EPA.DB/MSSQL/Models/University.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using AutoMapper;
+
+namespace EPA.DB.MSSQL.Models
+{
+    class University
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+        public string Site { get; set; }
+
+        public List<Facultaty> Facultaties { get; set; }
+
+        static University()
+        { Mapper.Initialize(cfg => cfg.CreateMap < University, CommonProfTestResult> ()); }
+    }
+}
