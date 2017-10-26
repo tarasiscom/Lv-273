215e9b8dc8e1dc100ee367367f4eedaa5c7ee249
Toregan@users.noreply.github.com <kachmar.vasil@gmail.com>
2017-10-26 20:24:37 +0300

    Change navigate menu.

--- src/EPA.Web/ClientApp/components/NavigationMenu.tsx
+++ src/EPA.Web/ClientApp/components/NavigationMenu.tsx
@@ -4,23 +4,31 @@ import { Link, NavLink } from 'react-router-dom';
 export class NavigationMenu extends React.Component<{}, {}> {
     public render() {
         return <div>
-        <nav className="navbar navbar-inverse ">
-            <div className = "container-fluid ">
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid ">
                     <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=". navbar-collapse">
+                            <span className="icon-bar"> </span>
+                            <span className="icon-bar"> </span>
+                            <span className="icon-bar"> </span>
+                        </button>
                         <Link className="navbar-brand" to={'/'}> EPA</Link>
+                    </div>
+                    <div className="navbar-collapse collapse">
+
+                        <ul className="nav navbar-nav">
+                            <li><Link to={'/'}>Усі Університети</Link></li>
+                            <li><Link to={'/profTest'}>Профорієнтаційні Тести</Link></li>
+                            <li><Link to={'/'}>Обрати Університет</Link></li>
+
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><Link to={'/'}>Вхід</Link></li>
+                            <li><Link to={'/'}>Реєстрація</Link></li>
+                        </ul>
+                    </div>
                 </div>
-                <ul className="nav navbar-nav">
-                    <li><Link  to={'/'}>Усі Університети</Link></li>
-                    <li><Link  to={'/profTest'}>Профорієнтаційні Тести</Link></li>
-                    <li><Link to={'/'}>Обрати Університет</Link></li>
-                    
-                </ul>
-                <ul className="nav navbar-nav navbar-right">
-                    <li><Link  to={'/'}>Вхід</Link></li>
-                    <li><Link to={'/'}>Реєстрація</Link></li>
-                </ul>
-            </div>
-        </nav>
+            </nav>
 </div>
     }
 }
