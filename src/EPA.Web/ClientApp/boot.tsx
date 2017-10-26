79f366e22d1d4c351ebde1afde9f1d13bcd183ab
Toregan@users.noreply.github.com <kachmar.vasil@gmail.com>
2017-10-26 20:28:35 +0300

    split file site.css

--- src/EPA.Web/ClientApp/boot.tsx
+++ src/EPA.Web/ClientApp/boot.tsx
@@ -1,4 +1,5 @@
-import './css/site.css';
+import './css/content.css';
+import './css/navbarAndFoter.css';
 import 'bootstrap';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
