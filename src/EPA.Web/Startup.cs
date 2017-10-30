0d4e0f7137088756d4d2cb22b3349839a976ed6a
Taras Yatsyshyn <yatarasis@gmail.com>
2017-10-30 18:01:09 +0200

    working version of EPA

--- src/EPA.Web/Startup.cs
+++ src/EPA.Web/Startup.cs
@@ -20,7 +20,6 @@ namespace EPA.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
             services.AddMvc();
             services.AddTransient<ITestProvider, ProfTestInfoProvider>();
             services.Configure<ConstSettings>(this.Configuration.GetSection("ConstSettings"));
