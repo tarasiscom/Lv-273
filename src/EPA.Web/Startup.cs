d6f3ce0e6bb18a819e4f87b54b6ff21f604b2f5e
Taras Yatsyshyn <yatarasis@gmail.com>
2017-10-30 17:17:20 +0200

    user secrets configuration in EPA.WEB

--- src/EPA.Web/Startup.cs
+++ src/EPA.Web/Startup.cs
@@ -6,22 +6,31 @@ using Microsoft.Extensions.DependencyInjection;
 using EPA.Common.Interfaces;
 using EPA.MSSQL.SQLDataAccess;
 
-
-
 namespace EPA.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             this.Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            this.Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        string testSecret = null;
+
+        public IConfiguration Configuration { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            testSecret = Configuration["ConnectionString"];
             services.AddMvc();
             services.AddTransient<ITestProvider, ProfTestInfoProvider>();
             services.Configure<ConstSettings>(this.Configuration.GetSection("ConstSettings"));
