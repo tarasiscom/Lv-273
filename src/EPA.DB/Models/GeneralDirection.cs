33d9cf738f8fff995c9acc3c0014e3dedce71bb5
Puhach Ivan <spoittles1@gmail.com>
2017-11-01 17:39:52 +0200

    Creating new Table GeneralDirection on the server db

---
+++ src/EPA.DB/Models/GeneralDirection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EPA.MSSQL.Models
+{
+    public class GeneralDirection
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Direction> Directions { get; set; }
+    }
+}
