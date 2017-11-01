e4fe20bfbfcac377d1c3c96cba9b308031723515
IKmet <irakmett@gmail.com>
2017-11-01 17:32:37 +0200

    add new interface which provides specialty methods

---
+++ src/EPA.Common/Interfaces/ISpecialtyProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using EPA.Common.DTO;
+
+namespace EPA.Common.Interfaces
+{
+    public interface ISpecialtyProvider
+    {
+        IEnumerable<Specialty> GetSpecialtiesByDirection(int idDirection);
+    }
+}
