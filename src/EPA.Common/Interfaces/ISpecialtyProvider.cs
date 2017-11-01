d93834603caca76dbf59b8439d736f8a022f66b7
IKmet <irakmett@gmail.com>
2017-11-01 17:58:42 +0200

    some comment

--- src/EPA.Common/Interfaces/ISpecialtyProvider.cs
+++ src/EPA.Common/Interfaces/ISpecialtyProvider.cs
@@ -3,6 +3,9 @@ using EPA.Common.DTO;
 
 namespace EPA.Common.Interfaces
 {
+    /// <summary>
+    /// This interface describes methods that returns data for choose specialty feature
+    /// </summary>
     public interface ISpecialtyProvider
     {
         IEnumerable<Specialty> GetSpecialtiesByDirection(int idDirection);
