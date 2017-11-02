53203e8855a9d9ac913ddf82a0577bdbd8fbcf6d
Puhach Ivan <spoittles1@gmail.com>
2017-11-02 14:58:22 +0200

    changed output of GetSpecialtiesByDirection method

--- src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
+++ src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
@@ -16,8 +16,23 @@ namespace EPA.MSSQL.SQLDataAccess
             this.context = cont;
         }
 
-        public IEnumerable<EPA.Common.DTO.Specialty> GetSpecialtiesByDirection(int idDirection) =>
-                            this.context.Specialties.Where(x => x.Direction.GeneralDirection.Id == idDirection).Select(x => x.ToCommon());
+        public IEnumerable<EPA.Common.DTO.Specialty> GetSpecialtiesByDirection(int idDirection)
+        {
+            return (from s in this.context.Specialties
+                    join u in this.context.Universities on s.University.Id equals u.Id
+                    where s.Direction.GeneralDirection.Id == idDirection
+                    select new Common.DTO.Specialty()
+                    {
+                        Name = s.Name,
+                        Address = u.Address,
+                        District = u.District,
+                        Site = u.Site,
+                        University = u.Name
+                    }).Distinct();
+
+                           //this.context.Specialties.Where(x => x.Direction.GeneralDirection.Id == idDirection).Select(x => x.ToCommon());
+        }
+
 
         public IEnumerable<EPA.Common.DTO.GeneralDirection> GetGeneralDirections() => this.context.GeneralDirections.Select(x => x.ToCommon());
 
