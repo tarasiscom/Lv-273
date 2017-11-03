40dd80f71fa1b9a8f4ab82f04f7e74e2c90fc5ea
Taras Yatsyshyn <yatarasis@gmail.com>
2017-11-03 17:29:04 +0200

    GetSpecialtyBySubjects new version

--- src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
+++ src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
@@ -33,14 +33,20 @@ namespace EPA.MSSQL.SQLDataAccess
 
         public IEnumerable<EPA.Common.DTO.GeneralDirection> GetGeneralDirections() => this.context.GeneralDirections.Select(x => x.ToCommon());
 
-        public IEnumerable<Common.DTO.Specialty> GetSpecialtyBySubjects(List<Common.DTO.Subject> listOfSubjects)
+        public IEnumerable<Common.DTO.Specialty> GetSpecialtyBySubjects(List<int> listOfSubjects)
         {
 
-            return (from sp in this.context.Specialties
-                    where sp. 
-                        (from ss in this.context.Specialty_Subjects
-                        where ss.Subject.Id in listOfSubjects.Select(x=>x.Id))
-                
+            return from s in this.context.Specialties
+                   join u in this.context.Universities on s.University.Id equals u.Id
+                   where (from ss in this.context.Specialty_Subjects where listOfSubjects.Contains(ss.Id) select ss.Id).Contains(s.Id)
+                   select new Common.DTO.Specialty()
+                   {
+                       Name = s.Name,
+                       Address = u.Address,
+                       District = u.District,
+                       Site = u.Site,
+                       University = u.Name
+                   };
         }
 
         public IEnumerable<Common.DTO.Subject> GetAllSubjects() => this.context.Subjects.Select(x => x.ToCommon());
