dfc2994ba362a9bfdd040e13d418533c5437fb28
VasiaKachmar <kachmar.vasil@gmail.com>
2017-11-03 12:43:05 +0200

    Change button to link and add margin to cards

--- src/EPA.Web/ClientApp/components/ChooseUniversity.tsx
+++ src/EPA.Web/ClientApp/components/ChooseUniversity.tsx
@@ -3,8 +3,7 @@ import { RouteComponentProps } from 'react-router';
 
 export class ChooseUniversity extends React.Component<RouteComponentProps<{}>, {}>
 {
-    renderBySpecialties() { /*Call render*/};
-    renderBySubjects() { /*Call render*/} 
+    
     renderSelect() {
         return <div>
             <div className="card col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2 col-xs-8 col-xs-offset-2 pad-for-nav">
@@ -12,9 +11,7 @@ export class ChooseUniversity extends React.Component<RouteComponentProps<{}>, {
                 <div className="card-block">
                     <h4 className="card-title">Пошук спеціальності по галузі</h4>
                     <p className="card-text">В цьому розділі можли підібрати університет по галузі і спеціальності яка подобається</p>
-                    <button className="btn btn-primary" onClick={() => this.renderBySpecialties()}>
-                        Розпочати
-                    </button>
+                    <a href="#" className="btn btn-primary">Рзпочати</a>
                 </div>
 
             </div>
@@ -22,10 +19,8 @@ export class ChooseUniversity extends React.Component<RouteComponentProps<{}>, {
                 <img className="card-img-top" src="/pictures/profPicture1.jpg" alt="Card image cap" width="100%" ></img>
                 <div className="card-block">
                     <h4 className="card-title">Пошук Спеціальності по предметах</h4>
-                    <p className="card-text">В Цьлму розділі допоможе підібрати по предметах які хочеш здавати на ЗНО</p>
-                    <button className="btn btn-primary" onClick={() => this.renderBySubjects()}>
-                        Розпочати
-                    </button>
+                    <p className="card-text">Цей розділі допоможе підібрати спеціальність по предметах які хочеш здавати на ЗНО</p>
+                    <a href="#" className="btn btn-primary">Рзпочати</a>
 
                 </div>
 
