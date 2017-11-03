b5a10a87a5eb03ae9eb1f50b5450c8d6c1b56f8d
astetskovych <astetskovych@gmail.com>
2017-11-03 17:29:49 +0200

    description for ChooseUniversity

--- src/EPA.Web/ClientApp/components/ChooseUniversity.tsx
+++ src/EPA.Web/ClientApp/components/ChooseUniversity.tsx
@@ -9,8 +9,10 @@ export class ChooseUniversity extends React.Component<RouteComponentProps<{}>, {
             <div className="card col-md-offset-2  col-md-3 col-sm-offset-1 col-sm-4  col-xs-8 col-xs-offset-2 pad-for-nav">
                 <img className="card-img-top" src="/pictures/profPicture2.jpg" alt="Card image cap" width="100%" ></img>
                 <div className="card-content">
-                    <h4 className="card-title">Пошук спеціальності по галузі</h4>
-                    <p className="card-text">В цьому розділі можли підібрати університет по галузі і спеціальності яка подобається</p>
+                    <h4 className="card-title">Пошук спеціальності за галуззю</h4>
+                    <p className="card-text">Цей розділ допоможе підібрати спеціальність у вищих навчальних закладах за галузями знань.
+                        Ви отримаєте інформацію про вищі навчальні заклади в яких проводиться набір на спеціальності за вибраною
+                        галуззю знань. Розділ допоможе вибрати ВНЗ за регіоном та рейтингом.</p>
                     <div className="card-read-more">
                         <a href="#" className="btn btn-link btn-block">  Розпочати      </a>
                     </div>
@@ -20,8 +22,10 @@ export class ChooseUniversity extends React.Component<RouteComponentProps<{}>, {
             <div className="card col-md-offset-2  col-md-3 col-sm-offset-1 col-sm-4 col-xs-8 col-xs-offset-2 pad-for-nav">
                 <img className="card-img-top" src="/pictures/profPicture1.jpg" alt="Card image cap" width="100%" ></img>
                 <div className="card-content">
-                    <h4 className="card-title">Пошук cпеціальності по предметах</h4>
-                    <p className="card-text">Цей розділі допоможе підібрати спеціальність по предметах які хочеш здавати на ЗНО</p>
+                    <h4 className="card-title">Пошук cпеціальності за предметами</h4>
+                    <p className="card-text">Цей розділ допоможе підібрати спеціальність у вищих навчальних закладах України за предметами ЗНО.
+                        Ви отримаєте інформацію про вищі навчальні заклади в яких проводиться набір на спеціальності за відповідними предметами ЗНО.
+                        Розділ допоможе вибрати ВНЗ за регіоном та рейтингом.</p>
                     <div className="card-read-more">
                         <a href="#" className="btn btn-link btn-block">  Розпочати      </a>
                     </div>
