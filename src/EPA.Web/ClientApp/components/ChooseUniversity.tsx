cbb6297789bec1663aed615084c249ba79f448a0
VasiaKachmar <kachmar.vasil@gmail.com>
2017-11-03 13:45:46 +0200

    Add stiles

--- src/EPA.Web/ClientApp/components/ChooseUniversity.tsx
+++ src/EPA.Web/ClientApp/components/ChooseUniversity.tsx
@@ -6,26 +6,27 @@ export class ChooseUniversity extends React.Component<RouteComponentProps<{}>, {
     
     renderSelect() {
         return <div>
-            <div className="card col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2 col-xs-8 col-xs-offset-2 pad-for-nav">
+            <div className="card col-md-offset-2  col-md-3 col-sm-offset-1 col-sm-4  col-xs-8 col-xs-offset-2 pad-for-nav">
                 <img className="card-img-top" src="/pictures/profPicture2.jpg" alt="Card image cap" width="100%" ></img>
-                <div className="card-block">
+                <div className="card-content">
                     <h4 className="card-title">Пошук спеціальності по галузі</h4>
                     <p className="card-text">В цьому розділі можли підібрати університет по галузі і спеціальності яка подобається</p>
-                    <a href="#" className="btn btn-primary">Рзпочати</a>
+                    <div className="card-read-more">
+                        <a href="#" className="btn btn-link btn-block">  Розпочати      </a>
+                    </div>
                 </div>
 
             </div>
-            <div className=" card col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2 col-xs-8 col-xs-offset-2 pad-for-nav">
+            <div className="card col-md-offset-2  col-md-3 col-sm-offset-1 col-sm-4 col-xs-8 col-xs-offset-2 pad-for-nav">
                 <img className="card-img-top" src="/pictures/profPicture1.jpg" alt="Card image cap" width="100%" ></img>
-                <div className="card-block">
-                    <h4 className="card-title">Пошук Спеціальності по предметах</h4>
+                <div className="card-content">
+                    <h4 className="card-title">Пошук cпеціальності по предметах</h4>
                     <p className="card-text">Цей розділі допоможе підібрати спеціальність по предметах які хочеш здавати на ЗНО</p>
-                    <a href="#" className="btn btn-primary">Рзпочати</a>
-
+                    <div className="card-read-more">
+                        <a href="#" className="btn btn-link btn-block">  Розпочати      </a>
+                    </div>
                 </div>
-
             </div>
-
         </div>
     }
     public render() {
