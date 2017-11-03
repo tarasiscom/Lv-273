d683fded68bb5eabb40bead3cb4b3a39751b2896
VasiaKachmar <kachmar.vasil@gmail.com>
2017-11-03 18:18:18 +0200

    Change

--- src/EPA.Web/ClientApp/components/ChooseSpecialityBuSubject.tsx
+++ src/EPA.Web/ClientApp/components/ChooseSpecialityBuSubject.tsx
@@ -1,6 +1,5 @@
 ﻿import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-//import logo from "ClientApp/images/epa1.png";
 
 export class ChooseSpecialityBuSubject extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
