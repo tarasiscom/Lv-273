61464b8a5bd837967ee9b9a9f9728fe01c126725
IKmet <irakmett@gmail.com>
2017-11-05 01:21:11 +0200

    configured Logger for keeping track sql queries

--- src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
+++ src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
@@ -5,6 +5,10 @@ using System.Text;
 using EPA.MSSQL.Models;
 using EPA.Common.Interfaces;
 using EPA.MSSQL.BusLogic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace EPA.MSSQL.SQLDataAccess
 {
@@ -39,20 +43,24 @@ namespace EPA.MSSQL.SQLDataAccess
 
         public IEnumerable<EPA.Common.DTO.Specialty> GetSpecialtiesByDirectionWithPagination(int idDirection, int page)
         {
+            var serviceProvider = context.GetInfrastructure<IServiceProvider>();
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.AddProvider(new MyLoggerProvider());
             int numberOfObjectsPerPage = 20;
-            return (from s in this.context.Specialties
-                    join u in this.context.Universities on s.University.Id equals u.Id
-                    join d in this.context.Districts on u.DistrictID equals d.Id
-                    where s.Direction.GeneralDirection.Id == idDirection
-                    select new Common.DTO.Specialty()
-                    {
-                        Name = s.Name,
-                        Address = u.Address,
-                        District = d.Name,
-                        Site = u.Site,
-                        Rating = CalculatingProvider.GetRating(s.NumApplication, s.NumEnrolled),
-                        University = u.Name
-                    }).Distinct().OrderByDescending(o => o.Rating).Skip((page - 1) * numberOfObjectsPerPage).Take(numberOfObjectsPerPage);
+            var qry = from s in this.context.Specialties
+                       join u in this.context.Universities on s.University.Id equals u.Id
+                       join d in this.context.Districts on u.DistrictID equals d.Id
+                       where s.Direction.GeneralDirection.Id == idDirection
+                       select new Common.DTO.Specialty()
+                       {
+                           Name = s.Name,
+                           Address = u.Address,
+                           District = d.Name,
+                           Site = u.Site,
+                           Rating = CalculatingProvider.GetRating(s.NumApplication, s.NumEnrolled),
+                           University = u.Name
+                       };
+            return qry.OrderByDescending(o => o.Rating).Skip((page - 1) * numberOfObjectsPerPage).Take(numberOfObjectsPerPage);
         }
     }
 }
