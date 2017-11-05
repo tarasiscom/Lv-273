cf94af0dc230f93acbd0b0f1e34fe45bd47204b1
Taras Yatsyshyn <yatarasis@gmail.com>
2017-11-05 19:01:21 +0200

    logger + getSP by subjects

--- src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
+++ src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
@@ -4,6 +4,11 @@ using EPA.Common.DTO;
 using EPA.Common.Interfaces;
 using EPA.MSSQL.Models;
 using Microsoft.Extensions.Options;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace EPA.MSSQL.SQLDataAccess
 {
@@ -35,36 +40,27 @@ namespace EPA.MSSQL.SQLDataAccess
 
         public IEnumerable<Common.DTO.Specialty> GetSpecialtyBySubjects(List<int> listOfSubjects)
         {
+            var serviceProvider = this.context.GetInfrastructure<IServiceProvider>();
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.AddProvider(new MyLoggerProvider());
 
-            return from s in this.context.Specialties
+            var q = (from ss in this.context.Specialty_Subjects
+                     group ss.Subject.Id by ss.Specialty.Id into grouped
+                     where grouped.All(item => listOfSubjects.Contains(item))
+                     select new { id = grouped.Key }).AsEnumerable().ToList();
+
+            return (from s in this.context.Specialties
                    join u in this.context.Universities on s.University.Id equals u.Id
-                   where (from ss in this.context.Specialty_Subjects where listOfSubjects.Contains(ss.Id) select ss.Id).Contains(s.Id)
+                //   where q.Contains(s.Id)
                    select new Common.DTO.Specialty()
                    {
                        Name = s.Name,
                        Address = u.Address,
-                       District = u.District,
+                      // District = u.District,
                        Site = u.Site,
                        University = u.Name
-                   };
+                   }).ToList();
 
-          /*  from s in this.context.Specialties
-            join u in this.context.Universities on s.University.Id equals u.Id
-            where
-                (from ss in this.context.Specialty_Subjects
-                where listOfSubjects.Contains(ss.Id)
-                group ss by new { ss.Specialty, ss.Subject } into la
-                where la.Count() >= listOfSubjects.Count
-                select la.Key.Specialty.Id).Contains(s.Id)
-                select new Common.DTO.Specialty()
-                {
-                    Name = s.Name,
-                    Address = u.Address,
-                    District = u.District,
-                    Site = u.Site,
-                    University = u.Name
-                };
-                */
         }
 
         public IEnumerable<Common.DTO.Subject> GetAllSubjects() => this.context.Subjects.Select(x => x.ToCommon());
