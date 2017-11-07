6e743fad8bb132f7cbbe24bdf03c969a4edffe02
Taras Yatsyshyn <yatarasis@gmail.com>
2017-11-07 15:58:46 +0200

    working ChooseSpecialtyBySubjects

--- src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
+++ src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
@@ -47,22 +47,42 @@ namespace EPA.MSSQL.SQLDataAccess
             var q = (from ss in this.context.Specialty_Subjects
                      group ss.Subject.Id by ss.Specialty.Id into grouped
                      where grouped.All(item => listOfSubjects.Contains(item))
-                     select new { id = grouped.Key }).AsEnumerable().ToList();
+                     select grouped.Key )
+                     .ToList();
 
             return (from s in this.context.Specialties
-                   join u in this.context.Universities on s.University.Id equals u.Id
-                //   where q.Contains(s.Id)
-                   select new Common.DTO.Specialty()
-                   {
-                       Name = s.Name,
-                       Address = u.Address,
-                      // District = u.District,
-                       Site = u.Site,
-                       University = u.Name
-                   }).ToList();
+                    join u in this.context.Universities on s.University.Id equals u.Id
+                    where q.Contains(s.Id)
+                    select new Common.DTO.Specialty()
+                    {
+                        Name = s.Name,
+                        Address = u.Address,
+                        // District = u.District,
+                        Site = u.Site,
+                        University = u.Name
+                    }).ToList();
+        }
 
+        public bool IsSubject(List<KeyId> q, int id)
+        {
+            bool king = false;
+            foreach (var tmp in q)
+            {
+                if (tmp.Id == id)
+                {
+                    king = true;
+                    break;
+                }
+            }
+
+            return king;
         }
 
         public IEnumerable<Common.DTO.Subject> GetAllSubjects() => this.context.Subjects.Select(x => x.ToCommon());
+    }
+
+    public class KeyId
+    {
+        public int Id { get; set; }
     }
 }
