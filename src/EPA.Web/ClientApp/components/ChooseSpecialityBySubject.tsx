801df8ae03f9d0b0d92ee3f14e7b62790ea752ec
VasiaKachmar <kachmar.vasil@gmail.com>
2017-11-10 11:25:59 +0200

    Add some style

--- src/EPA.Web/ClientApp/components/ChooseSpecialityBySubject.tsx
+++ src/EPA.Web/ClientApp/components/ChooseSpecialityBySubject.tsx
@@ -103,37 +103,37 @@ export class ChooseSpecialityBySubject extends React.Component<RouteComponentPro
 
         return <div className="col-md-offset-1  col-md-10  col-sm-10  col-xs-10 col-xs-offset-1 pagin">
             <div className="col-md-offset-1  col-md-10 col-sm-offset-1 col-sm-10  C col-xs-offset-1">
-                <div className="navigate">
-                    <div className="virtselect  col-md-4 col-sm-offset-1 col-sm-4  col-xs-8 col-xs-offset-2 pagin"><p>Предмети</p>
-                        <VirtualizedSelect multi={true} options={myList} onChange={(valueArray) => this.setState({ selectValueSub: valueArray })}
-                            value={this.state.selectValueSub}></VirtualizedSelect>
+                        <div className="navigate">
+                            <div className="virtselect  col-md-4 col-sm-offset-1 col-sm-4  col-xs-8 col-xs-offset-2 pagin"><p>Предмети</p>
+                                <VirtualizedSelect multi={true} options={myList} onChange={(valueArray) => this.setState({ selectValueSub: valueArray })}
+                                    value={this.state.selectValueSub}></VirtualizedSelect>
+                            </div>
+                            <div className="virtselect col-md-offset-1  col-md-3 col-sm-offset-1 col-sm-3  col-xs-8 col-xs-offset-2 pagin"><p>Області</p>
+                                <VirtualizedSelect multi={false} options={myListDisctict} onChange={(selectDistricty) => this.setState({ selectDistrict: selectDistricty })}
+                                    value={this.state.selectDistrict} ></VirtualizedSelect>
+                            </div>
+                            <button className="col-md-offset-1  col-md-2 col-sm-offset-1 col-sm-2  col-xs-8 col-xs-offset-2 btn btn-primary" onClick={() => this.submitFiltr(this.state.selectValueSub, this.state.selectDistrict)}> Пошук</button>
+                        </div>
+                        <div className="col-md-offset-1  col-md-10 col-sm-offset-1 col-sm-10  C col-xs-offset-1">
+                            <Tabbordion animateContent="height" className="accordion" mode="toggle" role="tablist">
+                                {this.state.univers.map(univer =>
+                                    <TabPanel >
+                                        <TabLabel className="glyphicon "><div className="glyphicon blockquote h4">
+                                            <p className="glyphicon glyphicon-menu-down">Спеціальність: {univer.name}</p>
+                                            <p>Університет:{univer.university}</p>
+                                            <p>Область:{univer.district}</p>
+                                        </div></TabLabel>
+                                        <TabContent>
+                                            <div>
+                                                <div className="col-md-6"><p>Адреса:{univer.address}</p> <p>Сайт:{univer.site}</p></div>
+                                                <div className="col-md-6"><ul>Предмети:{univer.subjects.map(sub => <li> {sub.name} </li>)} </ul></div>
+                                            </div>
+                                        </TabContent>
+                                    </TabPanel>
+                                )}    
+                            </Tabbordion>
                     </div>
-                    <div className="virtselect col-md-offset-1  col-md-3 col-sm-offset-1 col-sm-3  col-xs-8 col-xs-offset-2 pagin"><p>Області</p>
-                        <VirtualizedSelect multi={false} options={myListDisctict} onChange={(selectDistricty) => this.setState({ selectDistrict: selectDistricty })}
-                            value={this.state.selectDistrict} ></VirtualizedSelect>
-                    </div>
-                    <button className="col-md-offset-1  col-md-2 col-sm-offset-1 col-sm-2  col-xs-8 col-xs-offset-2 btn btn-primary" onClick={() => this.submitFiltr(this.state.selectValueSub, this.state.selectDistrict)}> Пошук</button>
-                </div>
-            <div className="col-md-offset-1  col-md-10 col-sm-offset-1 col-sm-10  C col-xs-offset-1">
-                <Tabbordion animateContent="height" className="accordion" mode="toggle" role="tablist">
-                    {this.state.univers.map(univer =>
-                        <TabPanel >
-                            <TabLabel className="glyphicon "><div className="glyphicon blockquote h4">
-                                <p className="glyphicon glyphicon-menu-down">Спеціальність: {univer.name}</p>
-                                <p>Університет:{univer.university}</p>
-                                <p>Область:{univer.district}</p>
-                            </div></TabLabel>
-                            <TabContent>
-                                <div>
-                                    <div className="col-md-6"><p>Адреса:{univer.address}</p> <p>Сайт:{univer.site}</p></div>
-                                    <div className="col-md-6"><ul>Предмети:{univer.subjects.map(sub => <li> {sub.name} </li>)} </ul></div>
-                                </div>
-                            </TabContent>
-                        </TabPanel>
-                    )}    
-                </Tabbordion>
-
-            </div>
+                        </div>
             <div className="col-md-6 col-sm-6 col-xs-12 pad-for-footer2"></div>
         </div>
     }
