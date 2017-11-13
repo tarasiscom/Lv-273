c6210e81d96d4a5ae7b987d4903b1e6fe027aada
Taras Yatsyshyn <yatarasis@gmail.com>
2017-11-13 12:03:39 +0200

    change variables names

--- src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
+++ src/EPA.DB/SQLDataAccess/SpecialtyProvider.cs
@@ -38,8 +38,8 @@ namespace EPA.MSSQL.SQLDataAccess
             {
                 var q = (from ss in this.context.Specialty_Subjects
                          group ss.Subject.Id by ss.Specialty.Id into grouped
-                         where listSubjectsAndDistrict.ListSubject.All(x => grouped.Contains(x)) &&
-                         grouped.Count() >= listSubjectsAndDistrict.ListSubject.Count()
+                         where listSubjectsAndDistrict.ListSubjects.All(x => grouped.Contains(x)) &&
+                         grouped.Count() >= listSubjectsAndDistrict.ListSubjects.Count()
                          select grouped.Key).ToList();
 
                 return this.GetSpecialty(listSubjectsAndDistrict, q);
@@ -49,8 +49,8 @@ namespace EPA.MSSQL.SQLDataAccess
                 var q = (from ss in this.context.Specialty_Subjects
                          where ss.Specialty.University.District.Id == listSubjectsAndDistrict.District
                          group ss.Subject.Id by ss.Specialty.Id into grouped
-                         where listSubjectsAndDistrict.ListSubject.All(x => grouped.Contains(x)) &&
-                         grouped.Count() >= listSubjectsAndDistrict.ListSubject.Count()
+                         where listSubjectsAndDistrict.ListSubjects.All(x => grouped.Contains(x)) &&
+                         grouped.Count() >= listSubjectsAndDistrict.ListSubjects.Count()
                          select grouped.Key).ToList();
 
                 return this.GetSpecialty(listSubjectsAndDistrict, q);
