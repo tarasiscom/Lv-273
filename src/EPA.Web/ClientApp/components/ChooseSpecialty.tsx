678eb1f837f148d841579386367b47d7bba03151
Nataliia Svystun <nat20088@ukr.net>
2017-11-17 16:52:28 +0200

    changed links

--- src/EPA.Web/ClientApp/components/ChooseSpecialty.tsx
+++ src/EPA.Web/ClientApp/components/ChooseSpecialty.tsx
@@ -15,7 +15,7 @@ export class ChooseSpecialty extends React.Component<RouteComponentProps<{}>, {}
                         Ви отримаєте інформацію про вищі навчальні заклади в яких проводиться набір на спеціальності за вибраною
                         галуззю знань. Розділ допоможе вибрати ВНЗ за регіоном та рейтингом.</p>
                     <div className="card-read-more">
-                        <Link to={'/ChooseSpecialties/ChooseSpecByDir'} className="btn btn-link btn-block">  Розпочати      </Link>
+                        <Link to={'/ChooseSpecialties/ByDirection'} className="btn btn-link btn-block">  Розпочати      </Link>
                     </div>
                 </div>
 
@@ -28,7 +28,7 @@ export class ChooseSpecialty extends React.Component<RouteComponentProps<{}>, {}
                         Ви отримаєте інформацію про вищі навчальні заклади в яких проводиться набір на спеціальності за відповідними предметами ЗНО.
                         Розділ допоможе вибрати ВНЗ за регіоном та рейтингом.</p>
                     <div className="card-read-more">
-                        <Link to="/ChooseSpecialties/ChooseSpecBySub" className="btn btn-link btn-block">  Розпочати      </Link>
+                        <Link to="/ChooseSpecialties/BySubject" className="btn btn-link btn-block">  Розпочати      </Link>
                     </div>
                 </div>
             </div>
