c6ea3870e5749c3149f688c4fc28eff4bc7b4126
Taras Yatsyshyn <yatarasis@gmail.com>
2017-11-23 17:27:14 +0200

    add rating field to table University

---
+++ src/EPA.DB/Migrations/20171123132445_UniversityRating.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EPA.MSSQL.Migrations
+{
+    public partial class UniversityRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Universities",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Universities");
+        }
+    }
+}
