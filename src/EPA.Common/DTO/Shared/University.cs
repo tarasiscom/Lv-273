261eb78f937ad1cff04bc632305a529a77b95745
Nataliia Svystun <nat20088@ukr.net>
2017-11-27 00:18:28 +0200

    add new class University in DTO

---
+++ src/EPA.Common/DTO/Shared/University.cs
@@ -0,0 +1,24 @@
+﻿namespace EPA.Common.DTO
+{
+    /// <summary>
+    ///  This class describes university
+    /// </summary>
+    public class University
+    {
+        /// <summary>
+        ///  ID of the university
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///  Name of the university
+        /// </summary>
+        public string Name { get; set; }
+        public string Site { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+
+        public byte[] Logo { get; set; }
+
+    }
+}
