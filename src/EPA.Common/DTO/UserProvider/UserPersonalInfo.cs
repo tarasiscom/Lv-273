7420381d820f78f020c234d7a411b7df4fa7e827
Kachmar Vasil <kachmar.vasil@gmail.com>
2017-11-27 16:19:33 +0200

    Add DTO UserPersonalInfo class Add interface IUserInformationProvider

---
+++ src/EPA.Common/DTO/UserProvider/UserPersonalInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPA.Common.DTO.UserProvider
+{
+    class UserPersonalInfo
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string District { get; set; }
+    }
+}
---
+++ src/EPA.Common/Interfaces/IUserInformationProvider.cs
@@ -0,0 +1,23 @@
+﻿using EPA.Common.DTO.UserProvider;
+using EPA.Common.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPA.Common.Interfaces
+{
+    interface IUserInformationProvider
+    {
+        /// <summary>
+        /// This method retrieves user personal information
+        /// </summary>
+        /// <returns>User personal information</returns>
+        UserPersonalInfo PersonalInfo();
+
+        /// <summary>
+        /// This method retrieves user favorites specialtys
+        /// </summary>
+        /// <returns>Favorites specialtys list</returns>
+        IEnumerable<Specialty> GetSpecialty();
+    }
+}
