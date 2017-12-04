bc01d751d35ecb8cb5d58c9329a3c2eda9fed379
IKmet <irakmett@gmail.com>
2017-12-04 17:50:33 +0200

    Fixed bug at Login API

--- src/EPA.Web/Controllers/AccountController.cs
+++ src/EPA.Web/Controllers/AccountController.cs
@@ -105,14 +105,15 @@ namespace EPA.Web.Controllers
         public StatusCodeResult LoginUser([FromBody]EPA.Common.DTO.LoginUser loginUser)
         {
             MSSQL.Models.User signedUser = userManager.FindByEmailAsync(loginUser.Email).GetAwaiter().GetResult();
-            var result = signInManager.PasswordSignInAsync(signedUser.UserName, loginUser.Password, isPersistent:true, lockoutOnFailure:false).GetAwaiter().GetResult();
-            if (result.Succeeded)
+            if (signedUser != null)
             {
-                return this.Ok();
+                var result = signInManager.PasswordSignInAsync(signedUser.UserName, loginUser.Password, isPersistent:true, lockoutOnFailure:false).GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    return this.Ok();
+                }
             }
             return this.BadRequest();
         }
-
-
     }
 }
